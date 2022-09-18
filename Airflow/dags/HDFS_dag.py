165372e109872005434d30d2aac99fbf6e4a1392
php1301 <19520854@gm.uit.edu.vn>
2022-09-17 21:33:35 -0700

    HDSF dag in celery local

--- Airflow/dags/HDFS_dag.py
+++ Airflow/dags/HDFS_dag.py
@@ -6,7 +6,7 @@ from airflow.operators.dummy import DummyOperator
 from airflow.utils.dates import days_ago
 
 default_args = {
-    'owner': 'php1301',
+    # 'owner': 'php1301',
     #'start_date': airflow.utils.dates.days_ago(2),
     # 'end_date': datetime(),
     # 'depends_on_past': False,
@@ -30,43 +30,43 @@ dag_execute_hdfs_commands = DAG(
 )
 
 start_task = BashOperator(task_id="start_task",
-                          bash_command="/opt/airflow/dags/test.sh ",
-                          dag=dag_execute_hdfs_commands
+                          bash_command="/home/phucpham1301/airflow/dags/test.sh ",
+                          dag=dag_execute_hdfs_commands,
                           )
 print("creating a directory")
 create_dir = BashOperator(task_id="create_dir",
-                          bash_command="/opt/airflow/hadoop/bin/hdfs dfs -mkdir /test_php1301  ",
+                          bash_command="/home/phucpham1301/hadoop-3.3.4/bin/hdfs dfs -mkdir -p /test_php1301  ",
                           dag=dag_execute_hdfs_commands
                           )
 
 print("giving permissions to a directory")
 give_permissions = BashOperator(
     task_id="give_permissions",
-    bash_command="/opt/airflow/hadoop/bin/hdfs dfs -chmod -R 777 /test_php1301 ",
-    dag=dag_execute_hdfs_commands,
-    run_as_user='root')
+    bash_command="/home/phucpham1301/hadoop-3.3.4/bin/hdfs dfs -chmod -R 777 /test_php1301 ",
+    dag=dag_execute_hdfs_commands
+    )
 
 list_all_files = BashOperator(task_id="list_files",
-                              bash_command="/opt/airflow/hadoop/bin/hdfs dfs -ls /  ",
-                              dag=dag_execute_hdfs_commands,
-                              run_as_user='root')
+                              bash_command="/home/phucpham1301/hadoop-3.3.4/bin/hdfs dfs -ls /  ",
+                              dag=dag_execute_hdfs_commands
+                              )
 
 create_empty_file = BashOperator(
     task_id="create_file",
-    bash_command="/opt/airflow/hadoop/bin/hdfs dfs -touchz /test_php1301/test.txt  ",
-    dag=dag_execute_hdfs_commands,
-    run_as_user='root')
+    bash_command="/home/phucpham1301/hadoop-3.3.4/bin/hdfs dfs -touchz /test_php1301/test.txt  ",
+    dag=dag_execute_hdfs_commands
+    )
 
 remove_dir = BashOperator(task_id="remove_dir",
-                          bash_command="/opt/airflow/hadoop/bin/hdfs dfs -rm -r /test_php1301  ",
+                          bash_command="/home/phucpham1301/hadoop-3.3.4/bin/hdfs dfs -rm -r /test_php1301  ",
                           dag=dag_execute_hdfs_commands,
-                          run_as_user='root')
+                          )
 copy_from_local = BashOperator(
     task_id="copy_from_local",
     bash_command=
-    "/opt/airflow/hadoop/bin/hdfs dfs -copyFromLocal /opt/airflow/hadoop/weatherHistory.csv /test_php1301  ",
-    dag=dag_execute_hdfs_commands,
-    run_as_user='root')
+    "/home/phucpham1301/hadoop-3.3.4/bin/hdfs dfs -copyFromLocal /home/phucpham1301/hadoop-3.3.4/weatherHistory.csv /test_php1301  ",
+    dag=dag_execute_hdfs_commands
+    )
 start_task >> create_dir >> give_permissions >> list_all_files >> create_empty_file >> remove_dir >> copy_from_local
 
 if __name__ == '__main__ ':
