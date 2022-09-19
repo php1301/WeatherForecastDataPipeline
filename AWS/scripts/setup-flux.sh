1634c658216d5106ba8d612f8830e7d29e98db6a
php1301 <19520854@gm.uit.edu.vn>
2022-09-19 03:11:57 +0000

    update flux setup script and modified airflow-dev release to match template/helper in airflow helm chart

--- AWS/scripts/setup-flux.sh
+++ AWS/scripts/setup-flux.sh
@@ -7,15 +7,16 @@ fi
 
 GIT_USERNAME=$1
 
-# installing Flux
+# Installing Flux
 # create the flux Kubernetes namespace
 kubectl create namespace flux
 
-# add the Flux chart repository to Helm and install Flux
+# add the Flux chart repository to Helm and install Flux.
 helm repo add fluxcd https://charts.fluxcd.io
 
 helm upgrade -i flux fluxcd/flux \
---set git.url=git@github.com:$GIT_USERNAME/airflow-eks-config \
+--set git.url=git@github.com:php1301/WeatherForecastDataPipeline \
+--set git.path="AWS/EKS/airflow-eks-config" \
 --namespace flux
 
 helm upgrade -i helm-operator fluxcd/helm-operator --wait \
